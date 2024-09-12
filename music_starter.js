be7cd8223802e1b3675c0c6d49d4ef54b457a01c
angieszn <miguelsiazon.ms@gmail.com>
2024-09-12 13:19:14 +1200

    Addition of rain-drops
    Adding in rain-drops / first test + with Wung's help of refining my code

--- music_starter.js
+++ music_starter.js
@@ -1,3 +1,42 @@
+class RainDrop {
+  constructor() {
+    this.x = random(width);
+    this.y = random(-100, -10);  // Start off-screen
+    this.z = random(0, 20);      // Different z-depth for perspective
+    this.len = map(this.z, 0, 20, 10, 20); // Length based on z-depth
+    this.yspeed = map(this.z, 0, 20, 1, 20); // Speed based on depth
+  }
+
+  fall() {
+    this.y += this.yspeed;  // Correct y movement direction
+    let grav = map(this.z, 0, 20, 0, 0.2);  // Apply gravity based on depth
+    this.yspeed += grav;  // Increase speed over time (gravity effect)
+
+    if (this.y > height) {
+      this.y = random(-100, -10);  // Reset raindrop position
+      this.yspeed = map(this.z, 0, 20, 4, 10);  // Reset speed
+    }
+  }
+
+  show() {
+    let thick = map(this.z, 0, 20, 1, 3);  // Thickness based on depth
+    strokeWeight(thick);
+    stroke(138, 43, 226);  // Color of raindrops
+    line(this.x, this.y, this.x, this.y + this.len);  // Draw raindrop as a line
+  }
+}
+
+let drops = [];  // Array to hold all raindrops
+
+function drawRain() {
+  for (let i = 0; i < drops.length; i++) {
+    drops[i].fall();  // Update position of raindrop
+    drops[i].show();  // Draw raindrop
+  }
+}
+
+let firstRun = true; // used for initializing
+
 let angle = 0;
 let r = 150;
 let vocal_history = [];
@@ -15,7 +54,16 @@ function add_to_history(history, d) {
 
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   
-  background(18, 7, 5);  // Clear the canvas with the background color
+  if (firstRun) { //initialize for raindrops
+    for (let i = 0; i < 500; i++) {  // increase/decrease of raindrops
+      drops.push(new RainDrop());
+    }
+    firstRun = false; // set to false so it doesn't run again.
+  }  
+
+  background(18, 7, 5);  // clears background
+  drawRain();  // Draws rain after background
+
   angleMode(RADIANS)
 
   add_to_history(vocal_history, vocal);
@@ -27,7 +75,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   add_to_history(other_history, other);
 
 
-  push(); //VOCAL part
+  push(); //VOCAL
   strokeWeight(5);
   stroke(99, 4, 7);
   translate(400, 400); //placement
@@ -38,7 +86,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     let angle = map(i, 0, 10, 0, PI * 2)
     let x = r * cos(angle);
     let y = r * sin(angle);
-    line(x, y, x*((historyVocal/100)+1),y*((historyVocal/100)+1) ) //math to allow for peaks
+    line(x, y, x*((historyVocal/100)+1),y*((historyVocal/100)+1) )
 
   }
   angle += 0.05; //random(-0.1, 0.1);
@@ -47,7 +95,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   pop(); // resets origin back to (0, 0)
 
 
-  push(); //DRUM PART
+  push(); //OTHER
   strokeWeight(5);
   stroke(209, 206, 209); 
   translate(400, 400);
@@ -66,7 +114,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
   pop(); 
 
-  push(); //BASS PART
+  push(); //BASS
   strokeWeight(5);
   stroke(146, 5, 16);
   translate(400, 400);
@@ -88,7 +136,7 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   pop(); 
 
  
-  push(); //OTHER PART
+  push();
   strokeWeight(5);
   stroke(215, 212, 213);
   translate(400, 400);
@@ -109,3 +157,118 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   r -= random(-2, 2);
 
 }
+
+
+
+
+// let angle = 0;
+// let r = 150;
+// let vocal_history = [];
+// let bass_history = [];
+// let drum_history = [];
+// let other_history = [];
+
+// function add_to_history(history, d) {
+//   history.push(d);
+//   if (history.length >= (width - 1) / 4) {
+//     history.shift();
+//   }
+// }
+
+
+// function draw_one_frame(words, vocal, drum, bass, other, counter) {
+  
+//   background(18, 7, 5);  // Clear the canvas with the background color
+//   angleMode(RADIANS)
+
+//   add_to_history(vocal_history, vocal);
+
+//   add_to_history(bass_history, bass);
+
+//   add_to_history(drum_history, drum);
+
+//   add_to_history(other_history, other);
+
+
+//   push(); //VOCAL part
+//   strokeWeight(5);
+//   stroke(99, 4, 7);
+//   translate(400, 400); //placement
+//   for (let i = 0; i <= 10; i++) {
+
+//     historyVocal = vocal_history[vocal_history.length - i]
+//     r = map(historyVocal, 0, 100, 0, 12)
+//     let angle = map(i, 0, 10, 0, PI * 2)
+//     let x = r * cos(angle);
+//     let y = r * sin(angle);
+//     line(x, y, x*((historyVocal/100)+1),y*((historyVocal/100)+1) ) //math to allow for peaks
+
+//   }
+//   angle += 0.05; //random(-0.1, 0.1);
+//   r -= random(-2, 2);
+
+//   pop(); // resets origin back to (0, 0)
+
+
+//   push(); //DRUM PART
+//   strokeWeight(5);
+//   stroke(209, 206, 209); 
+//   translate(400, 400);
+//   for (let i = 0; i <= 30; i++) {
+
+//     historyDrum = drum_history[drum_history.length - i]
+//     r = map(historyDrum, 0, 100, 30, 32)
+//     let angle = map(i, 0, 30, 0, PI * 2)
+//     let x = r * cos(angle);
+//     let y = r * sin(angle);
+//     line(x, y, x*((historyDrum/100)+1),y*((historyDrum/100)+1) )
+
+//   }
+//   angle += 0.05; 
+//   r -= random(-2, 2);
+
+//   pop(); 
+
+//   push(); //BASS PART
+//   strokeWeight(5);
+//   stroke(146, 5, 16);
+//   translate(400, 400);
+//   for (let i = 0; i <= 70; i++) {
+    
+//     historyBass = bass_history[bass_history.length - i]
+//     r = map(historyBass, 0, 100, 160, 160)
+//     let angle = map(i, 0, 70, 0, PI * 2)
+//     let x = r * cos(angle);
+//     let y = r * sin(angle);
+//     // point(x, y);
+//     // rect(x, y, 2, 4);
+//     line(x, y, x*((historyBass/100)+1),y*((historyBass/100)+1) )
+
+//   }
+//   angle += 0.05; 
+//   r -= random(-2, 2);
+
+//   pop(); 
+
+ 
+//   push(); //OTHER PART
+//   strokeWeight(5);
+//   stroke(215, 212, 213);
+//   translate(400, 400);
+//   for (let i = 0; i <= 100; i++) {
+
+//     historyOther = other_history[other_history.length - i]
+//     r = map(historyOther, 0, 100, 75, 75)
+//     let angle = map(i, 0, 100, 0, PI * 2)
+//     let x = r * cos(angle);
+//     let y = r * sin(angle);
+//     // point(x, y);
+//     // rect(x, y, 2, 4);
+//     line(x, y, x*((historyOther/100)+1),y*((historyOther/100)+1) )
+    
+
+//   }
+//   angle += 0.05; 
+//   r -= random(-2, 2);
+
+// }
